4b8cfe1f6df926ea7764d738f16f2d0277c02df3
JoshuaKornmayer <jbkornmayer80@gmail.com>
2020-04-08 08:57:16 -0400

    first commit with two exercise finished

---
+++ pizza.js
@@ -0,0 +1,9 @@
+
+function pizza(){
+let topping = ["Cheese", "Pepperoni", "Garlic", "Peppers"];
+
+return "A delicious pizza with " + topping[0] + " and " + topping[1] + " and " + topping[2] + " and " + topping[3] + "."
+};
+
+console.log(pizza());
+
