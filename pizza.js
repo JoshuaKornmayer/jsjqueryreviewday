57e6c6c5e5370b51ab857f788e54d36e1b333075
JoshuaKornmayer <jbkornmayer80@gmail.com>
2020-04-09 08:22:01 -0400

    updated pizza to be cleaner

--- pizza.js
+++ pizza.js
@@ -2,7 +2,10 @@
 function pizza(){
 let topping = ["Cheese", "Pepperoni", "Garlic", "Peppers"];
 
-return "A delicious pizza with " + topping[0] + " and " + topping[1] + " and " + topping[2] + " and " + topping[3] + "."
+var addToppings = topping.join(' and ');
+
+return "A delicious pizza that has " + addToppings;
+// return "A delicious pizza with " + topping[0] + " and " + topping[1] + " and " + topping[2] + " and " + topping[3] + "."
 };
 
 console.log(pizza());
